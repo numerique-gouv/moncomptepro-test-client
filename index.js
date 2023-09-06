ce8de4764b0edd30a2392e09cce46d1fdc69dc14
Raphaël Dubigny <rdubigny@gmail.com>
2023-09-06 02:10:45 +0200

    handle error + read env var PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cookieSession from "cookie-session";
 import morgan from "morgan";
 
 const app = express();
-const port = 3000;
+const port = parseInt(process.env.PORT, 10) || 3000;
 
 app.set("view engine", "ejs");
 app.use(
@@ -30,41 +30,54 @@ const getMcpClient = async () => {
   });
 };
 
-app.get("/", async (req, res) => {
-  res.render("index");
+app.get("/", async (req, res, next) => {
+  try {
+    res.render("index");
+  } catch (e) {
+    next(e);
+  }
 });
 
-app.post("/login", async (req, res) => {
-  const client = await getMcpClient();
-  const code_verifier = generators.codeVerifier();
-  req.session.verifier = code_verifier;
-  const code_challenge = generators.codeChallenge(code_verifier);
+app.post("/login", async (req, res, next) => {
+  try {
+    const client = await getMcpClient();
+    const code_verifier = generators.codeVerifier();
+    req.session.verifier = code_verifier;
+    const code_challenge = generators.codeChallenge(code_verifier);
 
-  const redirectUrl = client.authorizationUrl({
-    scope: process.env.MCP_SCOPES.replaceAll(",", " "),
-    code_challenge,
-    code_challenge_method: "S256",
-  });
+    const redirectUrl = client.authorizationUrl({
+      scope: process.env.MCP_SCOPES.replaceAll(",", " "),
+      code_challenge,
+      code_challenge_method: "S256",
+    });
 
-  res.redirect(redirectUrl);
+    res.redirect(redirectUrl);
+  } catch (e) {
+    next(e);
+  }
 });
 
-app.get("/login-callback", async (req, res) => {
-  const client = await getMcpClient();
-  const params = client.callbackParams(req);
-  const tokenSet = await client.callback(redirectUri, params, {
-    code_verifier: req.session.verifier,
-  });
+app.get("/login-callback", async (req, res, next) => {
+  try {
+    const client = await getMcpClient();
+    const params = client.callbackParams(req);
+    const tokenSet = await client.callback(redirectUri, params, {
+      code_verifier: req.session.verifier,
+    });
 
-  const userinfo = await client.userinfo(tokenSet.access_token);
+    const userinfo = await client.userinfo(tokenSet.access_token);
 
-  res.render("index", {
-    userinfo: JSON.stringify(userinfo, null, 2),
-    idtoken: JSON.stringify(tokenSet.claims(), null, 2),
-    oauth2token: JSON.stringify(tokenSet, null, 2),
-  });
+    res.render("index", {
+      userinfo: JSON.stringify(userinfo, null, 2),
+      idtoken: JSON.stringify(tokenSet.claims(), null, 2),
+      oauth2token: JSON.stringify(tokenSet, null, 2),
+    });
+  } catch (e) {
+    next(e);
+  }
 });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
+  console.log(process.env);
 });
