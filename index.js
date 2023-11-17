8973a8d7cbcd47c0a2024256ee0d95bded64d842
Raphaël Dubigny <rdubigny@gmail.com>
2023-11-17 17:28:18 +0100

    feat: add basic style, change org & logout button

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(
 );
 app.use(morgan("combined"));
 
-const redirectUri = `http://${process.env.HOST}:${process.env.PORT}${process.env.CALLBACK_URL}`;
+const origin = `http://${process.env.HOST}:${process.env.PORT}`;
+const redirectUri = `${origin}${process.env.CALLBACK_URL}`;
 
 const getMcpClient = async () => {
   const mcpIssuer = await Issuer.discover(process.env.MCP_PROVIDER);
@@ -34,7 +35,12 @@ const getMcpClient = async () => {
 
 app.get("/", async (req, res, next) => {
   try {
-    res.render("index");
+    res.render("index", {
+      title: process.env.SITE_TITLE,
+      userinfo: JSON.stringify(req.session.userinfo, null, 2),
+      idtoken: JSON.stringify(req.session.idtoken, null, 2),
+      oauth2token: JSON.stringify(req.session.oauth2token, null, 2),
+    });
   } catch (e) {
     next(e);
   }
@@ -51,6 +57,7 @@ app.post("/login", async (req, res, next) => {
       scope: process.env.MCP_SCOPES.replaceAll(",", " "),
       code_challenge,
       code_challenge_method: "S256",
+      login_hint: process.env.LOGIN_HINT,
     });
 
     res.redirect(redirectUrl);
@@ -59,7 +66,7 @@ app.post("/login", async (req, res, next) => {
   }
 });
 
-app.get("/login-callback", async (req, res, next) => {
+app.get(process.env.CALLBACK_URL, async (req, res, next) => {
   try {
     const client = await getMcpClient();
     const params = client.callbackParams(req);
@@ -67,13 +74,45 @@ app.get("/login-callback", async (req, res, next) => {
       code_verifier: req.session.verifier,
     });
 
-    const userinfo = await client.userinfo(tokenSet.access_token);
+    req.session.userinfo = await client.userinfo(tokenSet.access_token);
+    req.session.idtoken = tokenSet.claims();
+    req.session.oauth2token = tokenSet;
 
-    res.render("index", {
-      userinfo: JSON.stringify(userinfo, null, 2),
-      idtoken: JSON.stringify(tokenSet.claims(), null, 2),
-      oauth2token: JSON.stringify(tokenSet, null, 2),
+    res.redirect("/");
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.post("/select-organization", async (req, res, next) => {
+  try {
+    const client = await getMcpClient();
+    const code_verifier = generators.codeVerifier();
+    req.session.verifier = code_verifier;
+    const code_challenge = generators.codeChallenge(code_verifier);
+
+    const redirectUrl = client.authorizationUrl({
+      // scope: process.env.MCP_SCOPES.replaceAll(",", " "),
+      code_challenge,
+      code_challenge_method: "S256",
+      prompt: "select-organization",
     });
+
+    res.redirect(redirectUrl);
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.post("/logout", async (req, res, next) => {
+  try {
+    req.session = null;
+    const client = await getMcpClient();
+    const redirectUrl = client.endSessionUrl({
+      post_logout_redirect_uri: `${origin}/`,
+    });
+
+    res.redirect(redirectUrl);
   } catch (e) {
     next(e);
   }
